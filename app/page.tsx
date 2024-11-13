1cda11835a0df5103f2bb0c4adf65eacf625e274
nikakvinikadze <nikakvinikadze2007@gmail.com>
2024-11-13 20:28:33 +0400

    adding video

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,16 @@ export default function page() {
         </h1>
 
         <div className='flex items-center justify-center space-x-4'>
-          <button className='px-8 py-6 mx-4 bg-transparent border border-white text-xs text-white uppercase transition duration-200 hover:bg-white hover:text-black'>
-            our work{' '}
-          </button>
-          <button className='px-8 py-6 mx-4 bg-transparent border border-white text-xs text-white uppercase transition duration-200 hover:bg-white hover:text-black'>
-            our work{' '}
-          </button>
+          <div className=' min-h-[200px] flex items-center justify-center'>
+            <button className='px-6 py-2 font-medium bg-indigo-500 text-white w-fit transition-all shadow-[3px_3px_0px_black] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[3px]'>
+              Hover me
+            </button>
+          </div>
+          <div className=' min-h-[200px] flex items-center justify-center'>
+            <button className='px-6 py-2 font-medium bg-indigo-500 text-white w-fit transition-all shadow-[3px_3px_0px_black] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[3px]'>
+              Hover me
+            </button>
+          </div>
         </div>
       </div>
 
